ea9075bb14c7f3078fc0b45f91f7e0710f5ccebc
chy0010 <142566095+chy0010@users.noreply.github.com>
2023-11-18 14:18:43 -0600

    Add files via upload

---
+++ style.css
@@ -0,0 +1,24 @@
+
+      
+     
+   
+      
+  
+ 
+      
+      body {
+        background-color: #f8f8f8;
+        background-image: url("https://uploads-ssl.webflow.com/5e2520252e290c852ed89624/5e7ab4842c9596d555f88d6e_Gradien.png"), linear-gradient(#f8f8f8, #f8f8f8);
+        background-position: 0 0, 0 0;
+        background-repeat: no-repeat, repeat;
+      }
+      
+     
+      
+      
+    
+    
+    
+  
+
+    
